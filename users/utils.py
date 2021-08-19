1a9a245fa173ef8c3084c8228e1c8d7feafe789f
dwenpark <dwenup@gmail.com>
2021-08-19 11:14:08 +0900

    Add : 카카오 소셜로그인, 로그인 데코레이터, Test 코드
    - 카카오 소셜 로그인 뷰 작성
    - 로그인 데코레이터 작성
    - Test 코드 작성(성공, 실패, 예외)
    - third party error status 수정
    - 카카오 로그인 에러 예외 추가

---
+++ users/utils.py
@@ -0,0 +1,26 @@
+import jwt
+
+from django.http import JsonResponse
+
+from BeerBnB.settings import SECRET_KEY
+from .models          import User
+
+
+def login_decorator(func):
+    def wrapper(self, request):
+        try:
+            token   = request.headers.get("Authorization")
+            payload = jwt.decode(token, SECRET_KEY, algorithms = "HS256")
+
+            if not User.objects.filter(id = payload.get("id")).exists() :
+                return JsonResponse({"message" : "INVALID_MEMBER"}, status = 404)
+
+            user         = User.objects.get(id = payload["id"])
+            request.user = user
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({"message" : "INVALID_TOKEN"}, status = 403)
+
+        return func(self, request)
+
+        return wrapper
