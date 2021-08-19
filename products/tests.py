71c30bfe682eae62996f7eda4f5904b9517ce201
dwenpark <dwenup@gmail.com>
2021-08-19 15:53:02 +0900

    Add: 상품 리스트 검색 기능 구현
    - 주소 검색
    - 날짜 검색
    - 인원 검색
    - 테스트 코드 작성
    - Prefetch 객체 삭제 및 count if문 삭제

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,105 @@
-from django.test import TestCase
+from django.test      import TestCase, Client
 
-# Create your tests here.
+from .models          import Product, Image ,Category
+from users.models     import User
+from bookings.models  import Booking
+
+class ProductListTest(TestCase):
+    def setUp(self):
+        User.objects.bulk_create(
+            [User(
+                kakao     = 1234,
+                name      = "박종규",
+                thumbnail = "photo1",
+                birthday  = "2000-12-03",
+                is_host   = True),
+                User(
+                kakao     = 7777,
+                name      = "방문객",
+                thumbnail = "photo2",
+                birthday  = "2000-12-04",
+                is_host   = False)]
+        )
+        
+        Product.objects.bulk_create(
+            [Product(
+                name           = "1호집",
+                head_count     = 4,
+                latitude       = 1,
+                longitude      = 1,
+                price          = 100,
+                address        = "서울시 강남구",
+                detail_address = "1002호",
+                grade          = 0,
+                description    = "좋아요",
+                user_id        = User.objects.get(kakao=1234).id),
+                Product(
+                name           = "2호집",
+                head_count     = 2,
+                latitude       = 1,
+                longitude      = 1,
+                price          = 100,
+                address        = "서울시 강동구",
+                detail_address = "1003호",
+                grade          = 0,
+                description    = "좋아요",
+                user_id        = User.objects.get(kakao=1234).id)]
+        )
+        
+        Image.objects.bulk_create(
+            [Image(product_id = Product.objects.get(name="1호집").id, image = "1호점 사진1"),
+             Image(product_id = Product.objects.get(name="1호집").id, image = "1호점 사진2"),
+             Image(product_id = Product.objects.get(name="2호집").id, image = "2호점 사진1")])
+
+        Category.objects.bulk_create(
+            [Category(
+                product_id    = Product.objects.get(name="1호집").id,
+                big_address   = "서울시",
+                small_address = "강남구"),
+                Category(
+                product_id    = Product.objects.get(name="2호집").id,
+                big_address   = "서울시",
+                small_address = "강동구")]
+        )
+
+        Booking.objects.create(
+            product_id  = Product.objects.get(name="1호집").id,
+            user_id     = User.objects.get(kakao=7777).id,
+            check_in    = "2021-08-29",
+            check_out   = "2021-08-31",
+            head_count  = 1,
+            total_price = 100
+        )
+    
+    def tearDown(self):
+        Booking.objects.all().delete()
+        Image.objects.all().delete()
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+        User.objects.all().delete()
+        
+    def test_search_success(self):
+        client   = Client()
+        response = client.get("/products?address=서울시 강남구&checkin=2021-08-20&checkout=2021-08-21&count=3")
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_success_without_address(self) :
+        client   = Client()
+        response = client.get("/products?checkin=2021-08-20&checkout=2021-08-21&count=4")
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_success_overheadcount(self):
+        client   = Client()
+        response = client.get("/products?address=서울시&checkin=2021-08-20&checkout=2021-08-21&count=5")
+        self.assertEqual(response.json()["message"], [])
+        
+    def test_search_fail_without_checkout(self):
+        client   = Client()
+        response = client.get("/products?address=서울시&checkin=2021-08-20&count=3")
+        self.assertEqual(response.json()["message"], "NEED_DATE")
+        
+    def test_search_fail_valueerror(self):
+        client   = Client()
+        response = client.get("/products?address=서울시&checkin=2021-08-20&checkout=rrr&count=3")
+        self.assertEqual(response.json()["message"], "VALUE_ERROR")
+        
--- products/views.py
+++ products/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from datetime         import datetime, timedelta
 
-# Create your views here.
+from django.views     import View
+from django.db.models import Q, Prefetch
+from django.http      import JsonResponse
+
+from products.models  import Product
+
+class ProductsListView(View):
+    def get(self, request):
+        try:
+            # 주소 검색
+            address  = request.GET.get("address")
+            
+            q = Q()
+            
+            if address:
+                q &= Q(address__contains=address)
+            
+            address_filtered_products = Product.objects.filter(q)
+    
+            # 날짜 검색
+            checkin  = request.GET.get("checkin")
+            checkout = request.GET.get("checkout")
+            
+            if not checkin or not checkout:
+                return JsonResponse({"message" : "NEED_DATE"}, status = 400)
+            
+            new_checkout = datetime.strptime(checkout, "%Y-%m-%d").date() - timedelta(1)
+            
+            date_filtered_products = address_filtered_products.exclude(
+                (Q(booking__check_out__gt = checkin) & Q(booking__check_in__lte = checkin)) |
+                Q(booking__check_in__range = [checkin, new_checkout])
+            )
+            
+            # 인원 가능 검색
+            count = request.GET.get("count", 1)
+            
+            filtered_products = date_filtered_products.filter(head_count__gte = count).prefetch_related("image_set")
+            
+            products = [{
+                "id"        : product.id,
+                "name"      : product.name,
+                "count"     : product.head_count,
+                "latitude"  : product.latitude,
+                "longitude" : product.longitude,
+                "price"     : product.price,
+                "image"     : [product.image for product in product.image_set.all()]
+            } for product in filtered_products]
+            
+            return JsonResponse({"message" : products}, status = 200)
+        
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+        
+        except ValueError:
+            return JsonResponse({"message" : "VALUE_ERROR"}, status = 400)
