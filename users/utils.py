1c687159a73229f207fa76470004210243d2fbc3
dwenpark <dwenup@gmail.com>
2021-08-23 16:44:58 +0900

    Fix: Decorator indent 변경, 변수 추가

--- users/utils.py
+++ users/utils.py
@@ -7,7 +7,7 @@ from .models          import User
 
 
 def login_decorator(func):
-    def wrapper(self, request):
+    def wrapper(self, request, *args, **kwargs):
         try:
             token   = request.headers.get("Authorization")
             payload = jwt.decode(token, SECRET_KEY, algorithms = "HS256")
@@ -18,9 +18,9 @@ def login_decorator(func):
             user         = User.objects.get(id = payload["id"])
             request.user = user
 
+            return func(self, request, *args, **kwargs)
+        
         except jwt.exceptions.DecodeError:
             return JsonResponse({"message" : "INVALID_TOKEN"}, status = 403)
 
-        return func(self, request)
-
-        return wrapper
+    return wrapper
